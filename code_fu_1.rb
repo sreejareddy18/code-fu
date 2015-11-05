7f1e02a6168ea3a09441fea83d4c27d7c6a977cf
Sreeja Reddy <Sreeja.Reddy@Cerner.com>
2015-11-05 14:47:55 +0530

    fix code-fu_1

--- code_fu_1.rb
+++ code_fu_1.rb
@@ -1,18 +1,24 @@
 #Print the second highest element within a list of numbers input from the command line
 
-num = []
-i = 0
-ARGV.each { |x| num << x.to_i }
-
 def  sort(num)
-for j in 2..num.length
-  key = num[j]
-  i = j - 1
-  while i > 0 and num[i] > key
-    num[i+1] = num[i]
-    i = i - 1
+  for j in 2..(num.length-1)
+    key = num[j]
+    i = j - 1
+    while i > 0 and num[i] > key
+      num[i+1] = num[i]
+      i = i - 1
+    end
+    num[i+1] = key
   end
-  num[i+1] = key
-end 
-puts num
+  num = num.uniq && num.reverse
+  index = num.each_index.select{|i| num[i] >= 0}
+  value = index[1]
+  if num.length < 2
+    puts "#{num[0]}"
+  else
+    puts "second highest number is #{num[value]}"
+  end
 end
+numbers = gets
+num = numbers.split.map(&:to_i)
+sort(num)
